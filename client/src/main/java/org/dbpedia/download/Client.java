2982cd826321cb3113812a73be1726b117854009
Manas Ranjan Dikshit <manasdikshit48@gmail.com>
2025-07-19 00:08:20 +0530

    Update Client.java

--- client/src/main/java/org/dbpedia/download/Client.java
+++ client/src/main/java/org/dbpedia/download/Client.java
@@ -5,8 +5,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,6 +18,7 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.ParseException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -29,64 +30,60 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+public class Client {
 
+    private static String defaultTargetPath = "./data/";
 
-public class Client
-{
-	private static String defaultTargetPath = "./data/";
+    private static String defaultCollection = "https://databus.dbpedia.org/dbpedia/collections/latest-core";
 
-	private static String defaultCollection = "https://databus.dbpedia.org/dbpedia/collections/latest-core";
-	
-	private static String defaultSparqlEndpoint = "https://databus.dbpedia.org/sparql";
+    private static String defaultSparqlEndpoint = "https://databus.dbpedia.org/sparql";
 
-    public enum GraphMode  { 
-    	NO_GRAPH, 
-    	DOWNLOAD_URL
-	}
+    public enum GraphMode {
+        NO_GRAPH,
+        DOWNLOAD_URL
+    }
 
     /**
-	 * Quick and dirty implementation of a download client, downloading the contents of a 
-	 * Databus collection. Code is ugly and I know it! 
-	 * This download process will be replaced by the official databus download client as soon as it 
-	 * takes less time to install than writing this code :)
-	 * @param args
-	 */
-	public static void main( String[] args )
-	{
-		
-		Options options = new Options();
-		options.addOption("p", "path", true, "The data path");
-		options.addOption("c", "collection", true, "The Databus collection to be downloaded");
-		options.addOption("g", "graph-mode", true, "change the mode in which .graph files are created"); //TODO only one mode so far
-		options.addOption("s", "sparql-endpoint", true, "the target sparql endpoint"); //TODO only one mode so far
+     * Quick and dirty implementation of a download client, downloading the contents of a
+     * Databus collection. Code is ugly and I know it!
+     * This download process will be replaced by the official databus download client as soon as it
+     * takes less time to install than writing this code :)
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		String targetPath = defaultTargetPath;
-		String collection = defaultCollection;
-		String sparqlEndpoint = defaultSparqlEndpoint;
+        Options options = new Options();
+        options.addOption("p", "path", true, "The data path");
+        options.addOption("c", "collection", true, "The Databus collection to be downloaded");
+        options.addOption("g", "graph-mode", true, "change the mode in which .graph files are created");
+        options.addOption("s", "sparql-endpoint", true, "the target sparql endpoint");
 
-		GraphMode gmode = GraphMode.NO_GRAPH;
-		CommandLineParser cmdParser = new DefaultParser();
+        String targetPath = defaultTargetPath;
+        String collection = defaultCollection;
+        String sparqlEndpoint = defaultSparqlEndpoint;
 
-		try {
+        GraphMode gmode = GraphMode.NO_GRAPH;
+        CommandLineParser cmdParser = new DefaultParser();
 
-			CommandLine cmd = cmdParser.parse(options, args);
+        try {
+            CommandLine cmd = cmdParser.parse(options, args);
 
-			if(cmd.hasOption("p")) {
-				targetPath = cmd.getOptionValue("p");
-			}
+            if (cmd.hasOption("p")) {
+                targetPath = cmd.getOptionValue("p");
+            }
 
-			if(cmd.hasOption("c")) {
-				collection = cmd.getOptionValue("c");
-			}
-			
-			if(cmd.hasOption("s")) {
-				sparqlEndpoint = cmd.getOptionValue("s");
-			}	
+            if (cmd.hasOption("c")) {
+                collection = cmd.getOptionValue("c");
+            }
 
-			if(cmd.hasOption("g")) {
-				String mode = cmd.getOptionValue("g"); //TODO add support for more modes
-				
-                switch (mode) {
+            if (cmd.hasOption("s")) {
+                sparqlEndpoint = cmd.getOptionValue("s");
+            }
+
+            if (cmd.hasOption("g")) {
+                String mode = cmd.getOptionValue("g");
+
+                switch (mode.toLowerCase()) {
                     case "":
                     case "no":
                         gmode = GraphMode.NO_GRAPH;
@@ -95,152 +92,154 @@ public class Client
                         gmode = GraphMode.DOWNLOAD_URL;
                         break;
                     default:
-                        gmode = GraphMode.DOWNLOAD_URL;
+                        System.out.println("Unknown graph mode: " + mode + " - defaulting to NO_GRAPH");
+                        gmode = GraphMode.NO_GRAPH;
                 }
-			}
-			
-			if(!targetPath.endsWith("/")) {
-				targetPath += "/";
-			}
-			
-			File directory = new File(targetPath);
+            }
 
-			if(!directory.exists() && !directory.mkdir()) {
-				System.out.println("Target path " + targetPath + " could not be created.");
-				return;
-			}
-			
-			System.out.println("Loading collection " + collection);
-			
+            if (!targetPath.endsWith("/") && !targetPath.endsWith(File.separator)) {
+                targetPath += File.separator;
+            }
 
-			String query = get("GET", collection, "text/sparql");
-			
+            Path targetDir = Paths.get(targetPath);
 
-			System.out.println("Collections resolved to query:");
-			System.out.println(query);
-			
-			// depending on running system, daytime or weather condition, the query is either already URL encoded or still plain text
-			System.out.println("CHECKING FOR URLENCODED");
-			System.out.println("RESULT: " + isURLEncoded(query));
-			
-			if(!isURLEncoded(query)) {
-				query = URLEncoder.encode(query, "UTF-8");
-			}
-			
-			String queryResult = query(sparqlEndpoint, query);
-			
-			ArrayList<String> files = new ArrayList<String>();
-			
-			JSONObject obj = new JSONObject(queryResult);	
-			
-			JSONArray bindings = obj.getJSONObject("results").getJSONArray("bindings");
-			
-			for (int i = 0; i < bindings.length(); i++)
-			{
-				JSONObject binding = bindings.getJSONObject(i);
-				String key = binding.keys().next();
-				
-				JSONObject result = binding.getJSONObject(key);
-				files.add(result.getString("value"));
-			}
-			
-			for(String file : files) {
-				
-				System.out.println("Downloading file: " + file);
-				
-				String filename = file.substring(file.lastIndexOf('/') + 1);
-				
-				String prefix = filename;
-				String suffixes = "";
-				
-				if(filename.contains(".")) {
-			      		prefix = filename.substring(0,filename.indexOf('.'));
-					suffixes = filename.substring(filename.indexOf('.'));
-				}
-				
-          
-                String hash = DigestUtils.md5Hex(file).toUpperCase().substring(0,4);
-                String uniqname = prefix + "_" + hash + suffixes;
+            if (!Files.exists(targetDir)) {
+                Files.createDirectories(targetDir);
+            }
 
-                HttpClient instance = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
-                HttpResponse response = instance.execute(new HttpGet(file));
-                response.getEntity().writeTo(new FileOutputStream(Paths.get(targetPath + uniqname).toFile(),false));
-				
-				if(gmode != GraphMode.NO_GRAPH) {
-                    Files.write(Paths.get(targetPath + uniqname + ".graph"), file.getBytes("UTF-8"));
-				}
-				
-				System.out.println("File saved to " + targetPath + uniqname);
-			}
-			
-			System.out.println("Done.");
+            System.out.println("Loading collection " + collection);
 
-		} catch (org.apache.commons.cli.ParseException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            String query = get("GET", collection, "text/sparql");
 
-	}
+            if (query == null || query.isEmpty()) {
+                System.err.println("Failed to retrieve SPARQL query from collection URL.");
+                return;
+            }
 
-	private static String query(String endpoint, String query) throws ParseException, IOException {
-		
-		HttpClient client = HttpClientBuilder.create().build();
-		
-		String body = "default-graph-uri=&format=application%2Fsparql-results%2Bjson&query=" + query;
-		
-		HttpEntity entity = new ByteArrayEntity(body.getBytes("UTF-8"));
-		
-		HttpPost request = new HttpPost(endpoint);
-		request.setEntity(entity);
-		request.setHeader("Content-type", "application/x-www-form-urlencoded");
-		// request.addHeader("Accept",  accept);
-		HttpResponse response = client.execute(request);
-		HttpEntity responseEntity = response.getEntity();
-		
-		if(responseEntity != null) {
-		    return EntityUtils.toString(responseEntity);
-		}
-		return null;
+            System.out.println("Collections resolved to query:");
+            System.out.println(query);
 
-	}
+            // Depending on the system, daytime or weather condition, the query is either already URL encoded or still plain text
+            System.out.println("CHECKING FOR URLENCODED");
+            System.out.println("RESULT: " + isURLEncoded(query));
 
-	private static boolean isURLEncoded(String query) {
-		
-		Pattern hasWhites = Pattern.compile("\\s+");
-		Matcher matcher = hasWhites.matcher(query);
-		
-		return !matcher.find();
-		
-	}
+            if (!isURLEncoded(query)) {
+                query = URLEncoder.encode(query, "UTF-8");
+            }
 
-	private static String get(String method, String urlString, String accept) throws IOException {
+            String queryResult = query(sparqlEndpoint, query);
 
-		System.out.println(method + ": " + urlString + " / ACCEPT: " + accept);
-			
-		HttpClient client = HttpClientBuilder.create().build();
-		
-		if(method.equals("GET")) {
+            if (queryResult == null || queryResult.isEmpty()) {
+                System.err.println("SPARQL query returned no results.");
+                return;
+            }
 
-			HttpGet request = new HttpGet(urlString);
-			request.addHeader("Accept",  accept);
-			HttpResponse response = client.execute(request);
-			HttpEntity responseEntity = response.getEntity();
-			
-			if(responseEntity != null) {
-			    return EntityUtils.toString(responseEntity);
-			}
-		}
-	
-		return null;
-		
-	}
+            ArrayList<String> files = new ArrayList<>();
 
+            JSONObject obj = new JSONObject(queryResult);
+            JSONArray bindings = obj.getJSONObject("results").getJSONArray("bindings");
 
+            for (int i = 0; i < bindings.length(); i++) {
+                JSONObject binding = bindings.getJSONObject(i);
+                String key = binding.keys().next();
+                JSONObject result = binding.getJSONObject(key);
+                files.add(result.getString("value"));
+            }
+
+            HttpClient client = HttpClientBuilder.create()
+                    .setRedirectStrategy(new LaxRedirectStrategy())
+                    .build();
+
+            for (String file : files) {
+                System.out.println("Downloading file: " + file);
+
+                String filename = file.substring(file.lastIndexOf('/') + 1);
+
+                String prefix = filename;
+                String suffixes = "";
+
+                if (filename.contains(".")) {
+                    prefix = filename.substring(0, filename.indexOf('.'));
+                    suffixes = filename.substring(filename.indexOf('.'));
+                }
+
+                String hash = DigestUtils.md5Hex(file).toUpperCase();
+                String uniqname = prefix + "_" + hash.substring(0, 8) + suffixes;
+
+                Path outputPath = Paths.get(targetPath, uniqname);
+
+                HttpGet request = new HttpGet(file);
+                HttpResponse response = client.execute(request);
+
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode != HttpStatus.SC_OK) {
+                    System.err.println("Failed to download " + file + " - HTTP " + statusCode);
+                    continue;
+                }
+
+                try (InputStream in = response.getEntity().getContent();
+                     OutputStream out = new FileOutputStream(outputPath.toFile())) {
+                    in.transferTo(out);
+                }
+
+                if (gmode != GraphMode.NO_GRAPH) {
+                    Path graphPath = Paths.get(targetPath + uniqname + ".graph");
+                    Files.write(graphPath, file.getBytes("UTF-8"));
+                }
+
+                System.out.println("File saved to " + outputPath);
+            }
+
+            System.out.println("Done.");
+
+        } catch (org.apache.commons.cli.ParseException e1) {
+            System.err.println("Command-line argument parsing failed: " + e1.getMessage());
+        } catch (MalformedURLException e) {
+            System.err.println("Invalid file URL: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("IO error occurred: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Unexpected error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static String query(String endpoint, String query) throws ParseException, IOException {
+        HttpClient client = HttpClientBuilder.create().build();
+
+        String body = "default-graph-uri=&format=application%2Fsparql-results%2Bjson&query=" + query;
+        HttpEntity entity = new ByteArrayEntity(body.getBytes("UTF-8"));
+
+        HttpPost request = new HttpPost(endpoint);
+        request.setEntity(entity);
+        request.setHeader("Content-type", "application/x-www-form-urlencoded");
+
+        HttpResponse response = client.execute(request);
+        HttpEntity responseEntity = response.getEntity();
+
+        return (responseEntity != null) ? EntityUtils.toString(responseEntity) : null;
+    }
+
+    private static boolean isURLEncoded(String query) {
+        Pattern hasWhites = Pattern.compile("\\s+");
+        Matcher matcher = hasWhites.matcher(query);
+        return !matcher.find();
+    }
+
+    private static String get(String method, String urlString, String accept) throws IOException {
+        System.out.println(method + ": " + urlString + " / ACCEPT: " + accept);
+
+        HttpClient client = HttpClientBuilder.create().build();
+
+        if (method.equalsIgnoreCase("GET")) {
+            HttpGet request = new HttpGet(urlString);
+            request.addHeader("Accept", accept);
+            HttpResponse response = client.execute(request);
+            HttpEntity responseEntity = response.getEntity();
+            return (responseEntity != null) ? EntityUtils.toString(responseEntity) : null;
+        }
+
+        return null;
+    }
 }
-
